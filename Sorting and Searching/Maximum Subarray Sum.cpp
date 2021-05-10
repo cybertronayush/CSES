f876006e6fabad407b5e8c8b3a3753f1d1fb397d
cybertronayush <ayushcybertron1.8@gmail.com>
2021-05-11 00:32:30 +0530

    Create Maximum Subarray Sum.cpp

---
+++ Sorting and Searching/Maximum Subarray Sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+    lli maxSubArray(lli n,vector<lli>& nums) {
+        
+        lli sum = 0; 
+        lli X = INT_MIN;
+        
+        for(auto num : nums){
+            sum +=num;
+            X = max(X,sum);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return X;
+    }
+int main(){
+    
+    lli n;
+	cin>>n;
+	vector<lli>vec(n);
+	for(int i =0; i < n;i++)
+	{
+	    cin>>vec[i];
+	}
+	cout<<maxSubArray(n,vec);
+	return 0;
+
+}
